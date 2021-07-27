a90ef5732688c47fbe80cce49254ec6fef4072ad
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-07-27 09:25:36 +0530

    using the same user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const { isEmail } = require('validator');
-const userSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { isEmail } = require("validator");
+const userSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
       required: true,
@@ -23,25 +24,25 @@ const userSchema = new mongoose.Schema({
   },
   {
     timestamps: true,
-  })
-userSchema.pre('save', async function (next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
+  }
+);
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
 
-    next()
-})
+  next();
+});
 userSchema.statics.login = async function (email, password) {
-    const user = await this.findOne({ email });
-    if (user) {
-        const isAuthenticated = await bcrypt.compare(password, user.password);
-        if (isAuthenticated) {
-            return user;
-        }
-        throw Error('incorrect pwd');
+  const user = await this.findOne({ email });
+  if (user) {
+    const isAuthenticated = await bcrypt.compare(password, user.password);
+    if (isAuthenticated) {
+      return user;
     }
-    throw Error('incorrect email');
+    throw Error("incorrect pwd");
+  }
+  throw Error("incorrect email");
+};
 
-}
-
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("users", userSchema, "users");
 module.exports = User;
